123061f218ee41439662b159553f80d348f0e138
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-28 14:07:25 -0800

    moving suppressions to global

---
+++ DiscordBotFunctionApp/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "EA0004:Make types declared in an executable internal", Justification = "Must be public to work with Discord.Net", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.SubscriptionCommandModule")]
+[assembly: SuppressMessage("Resilience", "EA0014:The async method doesn't support cancellation", Justification = "Can't use CTs in command signatures as not compatible with injection model", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.SubscriptionCommandModule")]
