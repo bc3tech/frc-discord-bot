51c4e3a0142e009a7ba7b369b26073aea8f3026f
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-29 15:02:46 -0800

    flag URL lookup

--- DiscordBotFunctionApp/DiscordInterop/Utility.cs
+++ DiscordBotFunctionApp/DiscordInterop/Utility.cs
@@ -5,4 +5,10 @@ using Discord;
 internal static class Utility
 {
     public static RequestOptions CreateCancelRequestOptions(CancellationToken cancellationToken) => new() { CancelToken = cancellationToken };
+
+    public static string CreateCountryFlagUnicode(string countryCode) => countryCode.Length is not 2
+        ? throw new ArgumentException("Country code must be exactly 2 characters long", nameof(countryCode))
+        : $":flag_{countryCode.ToLowerInvariant()}:";
+
+    public static Uri CreateCountryFlagUrl(string countryCode) => new($"https://flagsapi.com/{countryCode}/shiny/32.png");
 }
