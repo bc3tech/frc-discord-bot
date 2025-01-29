219b74e8549d1ad18da8fb15896411295933b4bf
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-29 14:59:48 -0800

    handle sometimes-null-but-not-nullable-in-the-API-def values

--- DiscordBotFunctionApp/DiscordInterop/AutoCompleteHandlers.cs
+++ DiscordBotFunctionApp/DiscordInterop/AutoCompleteHandlers.cs
@@ -32,16 +32,18 @@ internal sealed class AutoCompleteHandlers
             var eventsRepo = services.GetService<EventRepository>();
             Debug.Assert(eventsRepo is not null);
             var frcEvents = await eventsRepo.GetEventsAsync(default).ConfigureAwait(false);
+#pragma warning disable EA0011 // Consider removing unnecessary conditional access operator (?) - found instances where, even though decorated with [JsonRequired] and not nullable, values were coming through as `null`
             return AutocompletionResult.FromSuccess(
                 frcEvents.Where(i => i.Key.Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
-                    || i.Value.Name.Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
+                    || i.Value.Name?.Contains(userSearchString, StringComparison.OrdinalIgnoreCase) is true
                     || i.Value.Year.ToString(CultureInfo.InvariantCulture).Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
-                    || i.Value.City.Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
-                    || i.Value.Country.Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
-                    || i.Value.StateProv.Contains(userSearchString, StringComparison.OrdinalIgnoreCase)
-                    || i.Value.LocationName.Contains(userSearchString, StringComparison.OrdinalIgnoreCase))
+                    || i.Value.City?.Contains(userSearchString, StringComparison.OrdinalIgnoreCase) is true
+                    || i.Value.Country?.Contains(userSearchString, StringComparison.OrdinalIgnoreCase) is true
+                    || i.Value.StateProv?.Contains(userSearchString, StringComparison.OrdinalIgnoreCase) is true
+                    || i.Value.LocationName?.Contains(userSearchString, StringComparison.OrdinalIgnoreCase) is true)
                 .Take(MAX_RESULTS)
                 .Select(i => new AutocompleteResult(Ellipsify(eventsRepo.GetLabelForEvent(i.Key)), i.Key)));
+#pragma warning restore EA0011 // Consider removing unnecessary conditional access operator (?)
         }
     }
 
