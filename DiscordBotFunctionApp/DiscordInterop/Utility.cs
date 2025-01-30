a48b9637a53a820141f15029d1d82c81c89f2be7
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-30 08:35:55 -0800

    ToPacificTime helper

--- DiscordBotFunctionApp/DiscordInterop/Utility.cs
+++ DiscordBotFunctionApp/DiscordInterop/Utility.cs
@@ -6,9 +6,11 @@ internal static class Utility
 {
     public static RequestOptions CreateCancelRequestOptions(CancellationToken cancellationToken) => new() { CancelToken = cancellationToken };
 
-    public static string CreateCountryFlagUnicode(string countryCode) => countryCode.Length is not 2
+    public static string CreateCountryFlagEmojiRef(string countryCode) => countryCode.Length is not 2
         ? throw new ArgumentException("Country code must be exactly 2 characters long", nameof(countryCode))
         : $":flag_{countryCode.ToLowerInvariant()}:";
 
-    public static Uri CreateCountryFlagUrl(string countryCode) => new($"https://flagsapi.com/{countryCode}/shiny/32.png");
+    public static Uri CreateCountryFlagUrl(string countryCode, ushort dimension = 32) => new($"https://flagsapi.com/{countryCode}/shiny/{dimension}.png");
+
+    public static DateTimeOffset ToPacificTime(this DateTimeOffset dateTimeOffset) => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTimeOffset, "Pacific Standard Time");
 }
