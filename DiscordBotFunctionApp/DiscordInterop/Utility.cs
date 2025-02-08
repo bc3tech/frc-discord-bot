bbde74b23bde6e3f5fae1b6f3d89e4a34613877b
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-02-08 12:51:56 -0800

    accounting for no color specified on a team

--- DiscordBotFunctionApp/DiscordInterop/Utility.cs
+++ DiscordBotFunctionApp/DiscordInterop/Utility.cs
@@ -13,4 +13,13 @@ internal static class Utility
     public static Uri CreateCountryFlagUrl(string countryCode, ushort dimension = 32) => new($"https://flagsapi.com/{countryCode}/shiny/{dimension}.png");
 
     public static DateTimeOffset ToPacificTime(this DateTimeOffset dateTimeOffset) => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(dateTimeOffset, "Pacific Standard Time");
+
+    public static Color? GetLightestColorOf(params Color?[] colors)
+    {
+        // Use the formula for relative luminance
+        var luminances = colors.Where(c => c is not null)
+            .Select(c => c!.Value)
+            .Select(c => (c, 0.2126 * c.R + 0.7152 * c.G + 0.0722 * c.B));
+        return luminances.Any() ? luminances.MaxBy(l => l.Item2).c : null;
+    }
 }
