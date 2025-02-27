e05262c10ad4231d5a5342b7ab58b5d08730db2f
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-02-27 14:52:22 -0800

    adding "development mode" watermark to footer

--- DiscordBotFunctionApp/DiscordInterop/Embeds/EmbedBuilderFactory.cs
+++ DiscordBotFunctionApp/DiscordInterop/Embeds/EmbedBuilderFactory.cs
@@ -7,5 +7,9 @@ internal sealed class EmbedBuilderFactory
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "DI-created singleton")]
     public EmbedBuilder GetBuilder() => new EmbedBuilder()
         .WithColor(255, 255, 01)
-        .WithFooter("Data provided by The Blue Alliance, FIRST, and Statbotics.io");
+        .WithFooter(
+#if DEBUG
+        "**DEVELOPMENT MODE** " +
+#endif
+        "Data provided by The Blue Alliance, FIRST, and Statbotics.io");
 }
