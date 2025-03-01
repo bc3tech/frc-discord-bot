6f506a53ccea06b0512f770f0ffec2d293e948d1
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-03-01 11:17:26 -0800

    adding support for threading

--- DiscordBotFunctionApp/TbaInterop/Models/Notifications/UpcomingMatch.cs
+++ DiscordBotFunctionApp/TbaInterop/Models/Notifications/UpcomingMatch.cs
@@ -4,4 +4,4 @@ using TheBlueAlliance.Model;
 
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Reduce boilerplate by just matching exact JSON body")]
 [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Reduce boilerplate by just matching exact JSON body")]
-internal sealed record UpcomingMatch(string event_key, string match_key, string? team_key, string event_name, string[] team_keys, int? scheduled_time, int? predicted_time, Webcast? webcast);
+internal sealed record UpcomingMatch(string event_key, string match_key, string? team_key, string event_name, string[] team_keys, int? scheduled_time, int? predicted_time, Webcast? webcast) : ThreadedEntity(event_key, match_key);
--- DiscordBotFunctionApp/TbaInterop/Models/WebhookMessages.cs
+++ DiscordBotFunctionApp/TbaInterop/Models/WebhookMessages.cs
@@ -14,4 +14,28 @@ internal sealed record WebhookMessage
     required public JsonElement MessageData { get; init; }
 
     public T? GetDataAs<T>() => MessageData.Deserialize<T>();
+
+    public bool ThreadReplies() => MessageType is NotificationType.event_match_video or NotificationType.match_score or NotificationType.match_video;
+
+    public (string PartitionKey, string RowKey)? GetThreadLocator()
+    {
+        ThreadedEntity? threadedEntity;
+        switch (this.MessageType)
+        {
+            case NotificationType.match_video:
+            case NotificationType.event_match_video:
+                threadedEntity = GetDataAs<MatchVideo>();
+                break;
+            case NotificationType.match_score:
+                threadedEntity = GetDataAs<MatchScore>();
+                break;
+            case NotificationType.upcoming_match:
+                threadedEntity = GetDataAs<UpcomingMatch>();
+                break;
+            default:
+                return null;
+        }
+
+        return threadedEntity is null ? null : (threadedEntity.PartitionKey, threadedEntity.RowKey);
+    }
 }
