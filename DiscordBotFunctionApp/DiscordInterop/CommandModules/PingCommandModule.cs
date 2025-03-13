aa89413c0fa5470255357b8cd3deb334d15c4942
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-03-13 08:13:57 -0700

    adding ping command

---
+++ DiscordBotFunctionApp/DiscordInterop/CommandModules/PingCommandModule.cs
@@ -0,0 +1,11 @@
+﻿namespace DiscordBotFunctionApp.DiscordInterop.CommandModules;
+
+using Discord.Interactions;
+
+using System.Threading.Tasks;
+
+public sealed class PingCommandModule : CommandModuleBase
+{
+    [SlashCommand("ping", "Pings the bot to check if it's alive")]
+    public async Task PingAsync() => await this.RespondAsync("Pong!", ephemeral: true).ConfigureAwait(false);
+}
--- DiscordBotFunctionApp/GlobalSuppressions.cs
+++ DiscordBotFunctionApp/GlobalSuppressions.cs
@@ -20,4 +20,7 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Performance", "EA0004:Make types declared in an executable internal", Justification = "Must be public to work with Discord.Net", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.ChatCommandModule")]
 [assembly: SuppressMessage("Resilience", "EA0014:The async method doesn't support cancellation", Justification = "Can't use CTs in command signatures as not compatible with injection model", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.ChatCommandModule")]
 
+[assembly: SuppressMessage("Performance", "EA0004:Make types declared in an executable internal", Justification = "Must be public to work with Discord.Net", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.PingCommandModule")]
+[assembly: SuppressMessage("Resilience", "EA0014:The async method doesn't support cancellation", Justification = "Can't use CTs in command signatures as not compatible with injection model", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.PingCommandModule")]
+
 [assembly: SuppressMessage("Performance", "EA0004:Make types declared in an executable internal", Justification = "Must be public to work with Discord.Net", Scope = "type", Target = "~T:DiscordBotFunctionApp.DiscordInterop.CommandModules.CommandModuleBase")]
