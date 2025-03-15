d1874467b8d16ed3973772c53a998ee308d4565d
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-03-15 14:34:45 -0700

    Setting an activitysource on fxn invocation
    trying to get exception, trace, etc. telemetry all linked together in appinsights. Right now, very hard to correlate some exceptions to the full run

--- DiscordBotFunctionApp/Functions/TbaWebhookHandler.cs
+++ DiscordBotFunctionApp/Functions/TbaWebhookHandler.cs
@@ -9,14 +9,22 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
+using System.Diagnostics;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 internal sealed class TbaWebhookHandler(DiscordMessageDispatcher dispatcher, ILogger<TbaWebhookHandler> logger)
 {
+    private static readonly ActivitySource activitySource = new(Assembly.GetExecutingAssembly().GetName().FullName);
+
     [Function("TbaWebhookHandler")]
-    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = "tba/webhook")] HttpRequestData req, CancellationToken cancellationToken)
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = "tba/webhook")] HttpRequestData req, FunctionContext ctx, CancellationToken cancellationToken)
     {
+        using var fxnRun = activitySource.StartActivity(ctx.FunctionDefinition.Name);
+        Debug.Assert(fxnRun is not null, "fxnRun is null - this should never happen");
+        fxnRun.AddTag(nameof(ctx.InvocationId), ctx.InvocationId);
+
         var bodyContent = await req.ReadAsStringAsync().ConfigureAwait(false);
         if (string.IsNullOrWhiteSpace(bodyContent))
         {
