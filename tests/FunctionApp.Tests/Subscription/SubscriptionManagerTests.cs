9a70f4fae196c38c340c612ba07ea3408b749b24
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-03-26 11:42:40 -0700

    Subscription Manager tests

---
+++ tests/FunctionApp.Tests/Subscription/SubscriptionManagerTests.cs
@@ -0,0 +1,182 @@
+namespace FunctionApp.Tests.Subscription;
+using Azure;
+using Azure.Data.Tables;
+
+using DiscordBotFunctionApp.Storage.TableEntities;
+using DiscordBotFunctionApp.Subscription;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+public class SubscriptionManagerTests
+{
+    private const ulong GuildId = 12345UL;
+    private const ulong ChannelId = 67890UL;
+    private const string EventName = "2025wabon";
+    private const string TeamKey = "frc2046";
+    private readonly Mock<TableClient> _mockTeamSubscriptions;
+    private readonly Mock<TableClient> _mockEventSubscriptions;
+    private readonly Mock<ILogger<SubscriptionManager>> _mockLogger;
+    private readonly SubscriptionManager _subscriptionManager;
+
+    public SubscriptionManagerTests()
+    {
+        _mockTeamSubscriptions = new Mock<TableClient>();
+        _mockEventSubscriptions = new Mock<TableClient>();
+        _mockLogger = new Mock<ILogger<SubscriptionManager>>();
+        _mockLogger.Setup(l => l.IsEnabled(It.IsAny<LogLevel>())).Returns(true);
+        _subscriptionManager = new SubscriptionManager(_mockTeamSubscriptions.Object, _mockEventSubscriptions.Object, _mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task GetSubscriptionsForGuildAsync_ShouldReturnSubscriptions()
+    {
+        // Arrange
+        var teamSubscriptionEntity = new TeamSubscriptionEntity { PartitionKey = TeamKey, RowKey = EventName, Subscribers = new() { { GuildId.ToString(), [ChannelId] } } };
+        var eventSubscriptionEntity = new EventSubscriptionEntity { PartitionKey = EventName, RowKey = TeamKey, Subscribers = new() { { GuildId.ToString(), [ChannelId] } } };
+
+        _mockTeamSubscriptions
+            .Setup(t => t.QueryAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<int?>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .Returns(AsyncPageable<TeamSubscriptionEntity>.FromPages([Page<TeamSubscriptionEntity>.FromValues([teamSubscriptionEntity], null, Mock.Of<Response>())]));
+
+        _mockEventSubscriptions
+            .Setup(e => e.QueryAsync<EventSubscriptionEntity>(It.IsAny<string>(), It.IsAny<int?>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .Returns(AsyncPageable<EventSubscriptionEntity>.FromPages([Page<EventSubscriptionEntity>.FromValues([eventSubscriptionEntity], null, Mock.Of<Response>())]));
+
+        // Act
+        var result = new List<NotificationSubscription>();
+        await foreach (var subscription in _subscriptionManager.GetSubscriptionsForGuildAsync(GuildId, CancellationToken.None))
+        {
+            result.Add(subscription);
+        }
+
+        // Assert
+        Assert.Equal(2, result.Count);
+    }
+
+    [Fact]
+    public async Task SaveSubscriptionAsync_ShouldSaveSubscription()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Mock.Of<Response<TeamSubscriptionEntity>>());
+
+        _mockTeamSubscriptions.Setup(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Mock.Of<Response>());
+
+        // Act
+        await _subscriptionManager.SaveSubscriptionAsync(subscription, CancellationToken.None);
+
+        // Assert
+        _mockTeamSubscriptions.Verify(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task RemoveSubscriptionAsync_ShouldRemoveSubscription()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Response.FromValue(new TeamSubscriptionEntity { PartitionKey = TeamKey, RowKey = EventName, Subscribers = new() { { subscription.GuildId!.Value.ToString(), [subscription.ChannelId] } } }, Mock.Of<Response>()));
+
+        _mockTeamSubscriptions.Setup(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Mock.Of<Response>());
+
+        // Act
+        await _subscriptionManager.RemoveSubscriptionAsync(subscription, CancellationToken.None);
+
+        // Assert
+        _mockTeamSubscriptions.Verify(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task SaveSubscriptionAsync_ShouldLogAndThrowException_WhenUpsertFails()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Mock.Of<Response<TeamSubscriptionEntity>>());
+
+        var errorResponse = new Mock<Response>();
+        errorResponse.SetupGet(r => r.IsError).Returns(true);
+        errorResponse.SetupGet(r => r.Status).Returns(500);
+        errorResponse.SetupGet(r => r.ReasonPhrase).Returns("Internal Server Error");
+
+        _mockTeamSubscriptions.Setup(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(errorResponse.Object);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<HttpProtocolException>(() => _subscriptionManager.SaveSubscriptionAsync(subscription, CancellationToken.None));
+        Assert.Equal(500, exception.ErrorCode);
+        _mockLogger.Verify(l => l.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception, string>>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task RemoveSubscriptionAsync_ShouldLogAndThrowException_WhenUpsertFails()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Response.FromValue<TeamSubscriptionEntity>(new() { PartitionKey = TeamKey, RowKey = EventName, Subscribers = new() { { GuildId.ToString(), [ChannelId] } } }, Mock.Of<Response>()));
+
+        var errorResponse = new Mock<Response>();
+        errorResponse.SetupGet(r => r.IsError).Returns(true);
+        errorResponse.SetupGet(r => r.Status).Returns(500);
+        errorResponse.SetupGet(r => r.ReasonPhrase).Returns("Internal Server Error");
+
+        _mockTeamSubscriptions.Setup(t => t.UpsertEntityAsync(It.IsAny<TeamSubscriptionEntity>(), TableUpdateMode.Replace, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(errorResponse.Object);
+
+        // Act & Assert
+        var exception = await Assert.ThrowsAsync<HttpProtocolException>(() => _subscriptionManager.RemoveSubscriptionAsync(subscription, CancellationToken.None));
+        Assert.Equal(500, exception.ErrorCode);
+        _mockLogger.Verify(l => l.Log(LogLevel.Error, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task SaveSubscriptionAsync_ShouldLogWhenSubscriptionAlreadyExists()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        var existingEntity = new TeamSubscriptionEntity() { PartitionKey = TeamKey, RowKey = EventName };
+        existingEntity.Subscribers.AddSubscription(GuildId, ChannelId);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Response.FromValue(existingEntity, Mock.Of<Response>()));
+
+        // Act
+        await _subscriptionManager.SaveSubscriptionAsync(subscription, CancellationToken.None);
+
+        // Assert
+        _mockLogger.Verify(l => l.Log(LogLevel.Warning, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task RemoveSubscriptionAsync_ShouldLogWhenNoSubscriptionsFound()
+    {
+        // Arrange
+        var subscription = new NotificationSubscription(GuildId, ChannelId, EventName, TeamKey);
+
+        _mockTeamSubscriptions.Setup(t => t.GetEntityIfExistsAsync<TeamSubscriptionEntity>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Mock.Of<Response<TeamSubscriptionEntity>>());
+
+        // Act
+        await _subscriptionManager.RemoveSubscriptionAsync(subscription, CancellationToken.None);
+
+        // Assert
+        _mockLogger.Verify(l => l.Log(LogLevel.Debug, It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+    }
+}
--- tests/FunctionApp.Tests/TbaInterop/Models/WebhookMessageTests.cs
+++ tests/FunctionApp.Tests/TbaInterop/Models/WebhookMessageTests.cs
@@ -1,4 +1,4 @@
-namespace FunctionApp.Tests.TbaInterop.Models;
+﻿namespace FunctionApp.Tests.TbaInterop.Models;
 
 using DiscordBotFunctionApp.TbaInterop.Models;
 using DiscordBotFunctionApp.TbaInterop.Models.Notifications;
@@ -10,6 +10,8 @@ using System.Text.Json;
 using TheBlueAlliance.Api;
 
 using Xunit;
+
+using Match = TheBlueAlliance.Model.Match;
 
 public class WebhookMessageTests
 {
@@ -101,6 +103,34 @@ public class WebhookMessageTests
     }
 
     [Fact]
+    public void GetThreadDetails_ShouldReturnCorrectDetails_ForUpcomingMatch()
+    {
+        // Arrange  
+        var jsonString = "{\"event_key\":\"2025iscmp\",\"match_key\":\"2025iscmp_qm41\",\"event_name\":\"FIRST Israel District Championship\",\"team_keys\":[\"frc4586\",\"frc5951\",\"frc8175\",\"frc2630\",\"frc9740\",\"frc1937\"],\"scheduled_time\":1742998140,\"predicted_time\":1742998930,\"webcast\":{\"type\":\"twitch\",\"channel\":\"firstisrael\",\"status\":\"online\",\"stream_title\":\"🎗️ 2025 FIRST Israel District Championship | FIRST Robotics Competition\",\"viewer_count\":558}}";
+        var jsonElement = JsonDocument.Parse(jsonString).RootElement;
+        var webhookMessage = new WebhookMessage
+        {
+            MessageType = NotificationType.upcoming_match,
+            MessageData = jsonElement
+        };
+
+        var matchJsonString = "{\"key\":\"2025iscmp_qm41\",\"event_key\":\"2025iscmp\",\"comp_level\":\"qm\",\"set_number\":1,\"match_number\":41,\"alliances\":{\"red\":{\"team_keys\":[\"frc2630\",\"frc9740\",\"frc1937\"],\"score\":114,\"surrogate_team_keys\":[],\"dq_team_keys\":[]},\"blue\":{\"team_keys\":[\"frc4586\",\"frc5951\",\"frc8175\"],\"score\":123,\"surrogate_team_keys\":[],\"dq_team_keys\":[]}},\"winning_alliance\":\"blue\",\"score_breakdown\":null,\"videos\":[],\"time\":1742998140,\"actual_time\":1742998743,\"predicted_time\":1742998991,\"post_result_time\":1742999056}\r\n";
+        var match = JsonSerializer.Deserialize<Match>(matchJsonString);
+
+        var services = new Mock<IServiceProvider>();
+        var matchApi = new Mock<IMatchApi>();
+        matchApi.Setup(api => api.GetMatch("2025iscmp_qm41", null)).Returns(match);
+        services.Setup(s => s.GetService(typeof(IMatchApi))).Returns(matchApi.Object);
+
+        // Act  
+        var result = webhookMessage.GetThreadDetails(services.Object);
+
+        // Assert  
+        Assert.NotNull(result);
+        Assert.Equal("FIRST Israel District Championship | Quals 1.41", result.Value.Title);
+    }
+
+    [Fact]
     public void GetThreadDetails_ShouldReturnNull_ForUnsupportedType()
     {
         // Arrange
@@ -119,5 +149,81 @@ public class WebhookMessageTests
 
         // Assert
         Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetThreadDetails_ShouldThrowDebugAssertException_ForBadData_MatchVideo()
+    {
+        // Arrange
+        var jsonString = "{\"event_key\": \"2025ncmec\", \"match_key\": \"2025ncmec_sf1m1\", \"event_name\": \"\", \"match\": {\"key\": \"2025ncmec_sf1m1\", \"event_key\": \"2025ncmec\", \"comp_level\": \"sf\", \"set_number\": 1, \"match_number\": 1, \"alliances\": {\"red\": {\"team_keys\": [\"frc9496\", \"frc9198\", \"frc8738\"], \"score\": 203, \"surrogate_team_keys\": [], \"dq_team_keys\": []}, \"blue\": {\"team_keys\": [\"frc6932\", \"frc10107\", \"frc7890\"], \"score\": 73, \"surrogate_team_keys\": [], \"dq_team_keys\": []}}, \"winning_alliance\": \"red\", \"score_breakdown\": {\"red\": {\"autoLineRobot1\": \"Yes\", \"endGameRobot1\": \"Parked\", \"autoLineRobot2\": \"Yes\", \"endGameRobot2\": \"Parked\", \"autoLineRobot3\": \"Yes\", \"endGameRobot3\": \"Parked\", \"autoReef\": {\"topRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": true, \"nodeK\": false, \"nodeL\": true}, \"midRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"botRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"trough\": 1, \"tba_botRowCount\": 0, \"tba_midRowCount\": 0, \"tba_topRowCount\": 2}, \"autoCoralCount\": 3, \"autoMobilityPoints\": 9, \"autoPoints\": 26, \"autoCoralPoints\": 17, \"teleopReef\": {\"topRow\": {\"nodeA\": true, \"nodeB\": true, \"nodeC\": true, \"nodeD\": true, \"nodeE\": true, \"nodeF\": true, \"nodeG\": true, \"nodeH\": true, \"nodeI\": true, \"nodeJ\": true, \"nodeK\": true, \"nodeL\": true}, \"midRow\": {\"nodeA\": true, \"nodeB\": true, \"nodeC\": true, \"nodeD\": true, \"nodeE\": true, \"nodeF\": true, \"nodeG\": true, \"nodeH\": true, \"nodeI\": true, \"nodeJ\": true, \"nodeK\": true, \"nodeL\": true}, \"botRow\": {\"nodeA\": false, \"nodeB\": true, \"nodeC\": true, \"nodeD\": true, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": true, \"nodeJ\": true, \"nodeK\": true, \"nodeL\": true}, \"trough\": 1, \"tba_botRowCount\": 7, \"tba_midRowCount\": 12, \"tba_topRowCount\": 12}, \"teleopCoralCount\": 30, \"teleopPoints\": 177, \"teleopCoralPoints\": 121, \"algaePoints\": 50, \"netAlgaeCount\": 8, \"wallAlgaeCount\": 3, \"endGameBargePoints\": 6, \"autoBonusAchieved\": false, \"coralBonusAchieved\": false, \"bargeBonusAchieved\": false, \"coopertitionCriteriaMet\": false, \"foulCount\": 0, \"techFoulCount\": 0, \"g206Penalty\": false, \"g410Penalty\": false, \"g418Penalty\": false, \"g428Penalty\": false, \"adjustPoints\": 0, \"foulPoints\": 0, \"rp\": 0, \"totalPoints\": 203}, \"blue\": {\"autoLineRobot1\": \"Yes\", \"endGameRobot1\": \"Parked\", \"autoLineRobot2\": \"Yes\", \"endGameRobot2\": \"Parked\", \"autoLineRobot3\": \"Yes\", \"endGameRobot3\": \"None\", \"autoReef\": {\"topRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"midRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"botRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"trough\": 2, \"tba_botRowCount\": 0, \"tba_midRowCount\": 0, \"tba_topRowCount\": 0}, \"autoCoralCount\": 2, \"autoMobilityPoints\": 9, \"autoPoints\": 15, \"autoCoralPoints\": 6, \"teleopReef\": {\"topRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"midRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"botRow\": {\"nodeA\": false, \"nodeB\": false, \"nodeC\": false, \"nodeD\": false, \"nodeE\": false, \"nodeF\": false, \"nodeG\": false, \"nodeH\": false, \"nodeI\": false, \"nodeJ\": false, \"nodeK\": false, \"nodeL\": false}, \"trough\": 7, \"tba_botRowCount\": 0, \"tba_midRowCount\": 0, \"tba_topRowCount\": 0}, \"teleopCoralCount\": 7, \"teleopPoints\": 58, \"teleopCoralPoints\": 14, \"algaePoints\": 40, \"netAlgaeCount\": 4, \"wallAlgaeCount\": 4, \"endGameBargePoints\": 4, \"autoBonusAchieved\": false, \"coralBonusAchieved\": false, \"bargeBonusAchieved\": false, \"coopertitionCriteriaMet\": false, \"foulCount\": 0, \"techFoulCount\": 0, \"g206Penalty\": false, \"g410Penalty\": false, \"g418Penalty\": false, \"g428Penalty\": false, \"adjustPoints\": 0, \"foulPoints\": 0, \"rp\": 0, \"totalPoints\": 73}}, \"videos\": [{\"type\": \"youtube\", \"key\": \"ezKYM6QWzWI\"}], \"time\": 1742749200, \"actual_time\": 1742749972, \"predicted_time\": 1742748738, \"post_result_time\": 1742750178}}";
+        var jsonElement = JsonDocument.Parse(jsonString).RootElement;
+        var webhookMessage = new WebhookMessage
+        {
+            MessageType = NotificationType.match_video,
+            MessageData = jsonElement
+        };
+
+        var services = new Mock<IServiceProvider>();
+
+        // Act & Assert
+        AssertDebugException(() => webhookMessage.GetThreadDetails(services.Object), "Bad data!");
+    }
+
+    [Fact]
+    public void GetThreadDetails_ShouldThrowDebugAssertException_ForBadData_MatchScore()
+    {
+        // Arrange
+        var jsonString = "{\"event_key\":\"2025hiho\",\"match_key\":\"2025hiho_sf8m1\",\"event_name\":null,\"match\":{\"key\":\"2025hiho_sf8m1\",\"event_key\":\"2025hiho\",\"comp_level\":\"sf\",\"set_number\":8,\"match_number\":1,\"alliances\":{\"red\":{\"team_keys\":[\"frc4253\",\"frc3501\",\"frc10384\"],\"score\":117,\"surrogate_team_keys\":[],\"dq_team_keys\":[]},\"blue\":{\"team_keys\":[\"frc2438\",\"frc2477\",\"frc3882\"],\"score\":135,\"surrogate_team_keys\":[],\"dq_team_keys\":[]}},\"winning_alliance\":\"blue\",\"score_breakdown\":{\"red\":{\"autoLineRobot1\":\"Yes\",\"endGameRobot1\":\"DeepCage\",\"autoLineRobot2\":\"Yes\",\"endGameRobot2\":\"None\",\"autoLineRobot3\":\"Yes\",\"endGameRobot3\":\"Parked\",\"autoReef\":{\"topRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":true,\"nodeE\":true,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":true,\"nodeK\":false,\"nodeL\":false},\"midRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"botRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"trough\":0,\"tba_botRowCount\":0,\"tba_midRowCount\":0,\"tba_topRowCount\":3},\"autoCoralCount\":3,\"autoMobilityPoints\":9,\"autoPoints\":30,\"autoCoralPoints\":21,\"teleopReef\":{\"topRow\":{\"nodeA\":true,\"nodeB\":true,\"nodeC\":true,\"nodeD\":true,\"nodeE\":true,\"nodeF\":true,\"nodeG\":true,\"nodeH\":true,\"nodeI\":true,\"nodeJ\":true,\"nodeK\":true,\"nodeL\":true},\"midRow\":{\"nodeA\":true,\"nodeB\":true,\"nodeC\":true,\"nodeD\":true,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":true,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":true,\"nodeL\":true},\"botRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"trough\":0,\"tba_botRowCount\":0,\"tba_midRowCount\":7,\"tba_topRowCount\":12},\"teleopCoralCount\":16,\"teleopPoints\":87,\"teleopCoralPoints\":73,\"algaePoints\":0,\"netAlgaeCount\":0,\"wallAlgaeCount\":0,\"endGameBargePoints\":14,\"autoBonusAchieved\":false,\"coralBonusAchieved\":false,\"bargeBonusAchieved\":false,\"coopertitionCriteriaMet\":false,\"foulCount\":0,\"techFoulCount\":1,\"g206Penalty\":false,\"g410Penalty\":false,\"g418Penalty\":false,\"g428Penalty\":false,\"adjustPoints\":0,\"foulPoints\":0,\"rp\":0,\"totalPoints\":117},\"blue\":{\"autoLineRobot1\":\"Yes\",\"endGameRobot1\":\"None\",\"autoLineRobot2\":\"Yes\",\"endGameRobot2\":\"Parked\",\"autoLineRobot3\":\"Yes\",\"endGameRobot3\":\"DeepCage\",\"autoReef\":{\"topRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":true,\"nodeF\":false,\"nodeG\":false,\"nodeH\":true,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"midRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"botRow\":{\"nodeA\":false,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":false,\"nodeL\":false},\"trough\":1,\"tba_botRowCount\":0,\"tba_midRowCount\":0,\"tba_topRowCount\":2},\"autoCoralCount\":3,\"autoMobilityPoints\":9,\"autoPoints\":26,\"autoCoralPoints\":17,\"teleopReef\":{\"topRow\":{\"nodeA\":true,\"nodeB\":true,\"nodeC\":true,\"nodeD\":true,\"nodeE\":true,\"nodeF\":false,\"nodeG\":true,\"nodeH\":true,\"nodeI\":false,\"nodeJ\":true,\"nodeK\":true,\"nodeL\":true},\"midRow\":{\"nodeA\":true,\"nodeB\":true,\"nodeC\":true,\"nodeD\":true,\"nodeE\":false,\"nodeF\":false,\"nodeG\":true,\"nodeH\":true,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":true,\"nodeL\":true},\"botRow\":{\"nodeA\":true,\"nodeB\":false,\"nodeC\":false,\"nodeD\":false,\"nodeE\":false,\"nodeF\":false,\"nodeG\":false,\"nodeH\":false,\"nodeI\":false,\"nodeJ\":false,\"nodeK\":true,\"nodeL\":true},\"trough\":4,\"tba_botRowCount\":3,\"tba_midRowCount\":8,\"tba_topRowCount\":10},\"teleopCoralCount\":23,\"teleopPoints\":103,\"teleopCoralPoints\":89,\"algaePoints\":0,\"netAlgaeCount\":0,\"wallAlgaeCount\":0,\"endGameBargePoints\":14,\"autoBonusAchieved\":false,\"coralBonusAchieved\":false,\"bargeBonusAchieved\":false,\"coopertitionCriteriaMet\":false,\"foulCount\":0,\"techFoulCount\":0,\"g206Penalty\":false,\"g410Penalty\":false,\"g418Penalty\":false,\"g428Penalty\":false,\"adjustPoints\":0,\"foulPoints\":6,\"rp\":0,\"totalPoints\":135}},\"videos\":[],\"time\":1742776380,\"actual_time\":1742776993,\"predicted_time\":1742777045,\"post_result_time\":1742777192}}";
+        var jsonElement = JsonDocument.Parse(jsonString).RootElement;
+        var webhookMessage = new WebhookMessage
+        {
+            MessageType = NotificationType.match_score,
+            MessageData = jsonElement
+        };
+
+        var services = new Mock<IServiceProvider>();
+
+        // Act & Assert
+        AssertDebugException(() => webhookMessage.GetThreadDetails(services.Object), "Bad data!");
+    }
+
+    [Fact]
+    public void GetThreadDetails_ShouldThrowDebugAssertException_ForBadData_UpcomingMatch()
+    {
+        // Arrange
+        var jsonString = "{\"event_key\":\"2025iscmp\",\"match_key\":\"2025iscmp_qm41\",\"event_name\":\"\",\"team_keys\":[\"frc4586\",\"frc5951\",\"frc8175\",\"frc2630\",\"frc9740\",\"frc1937\"],\"scheduled_time\":1742998140,\"predicted_time\":1742998930,\"webcast\":{\"type\":\"twitch\",\"channel\":\"firstisrael\",\"status\":\"online\",\"stream_title\":\"🎗️ 2025 FIRST Israel District Championship | FIRST Robotics Competition\",\"viewer_count\":558}}";
+        var jsonElement = JsonDocument.Parse(jsonString).RootElement;
+        var webhookMessage = new WebhookMessage
+        {
+            MessageType = NotificationType.upcoming_match,
+            MessageData = jsonElement
+        };
+
+        var matchJsonString = "{\"key\":\"2025iscmp_qm41\",\"event_key\":\"2025iscmp\",\"comp_level\":\"qm\",\"set_number\":1,\"match_number\":41,\"alliances\":{\"red\":{\"team_keys\":[\"frc2630\",\"frc9740\",\"frc1937\"],\"score\":114,\"surrogate_team_keys\":[],\"dq_team_keys\":[]},\"blue\":{\"team_keys\":[\"frc4586\",\"frc5951\",\"frc8175\"],\"score\":123,\"surrogate_team_keys\":[],\"dq_team_keys\":[]}},\"winning_alliance\":\"blue\",\"score_breakdown\":null,\"videos\":[],\"time\":1742998140,\"actual_time\":1742998743,\"predicted_time\":1742998991,\"post_result_time\":1742999056}\r\n";
+        var match = JsonSerializer.Deserialize<Match>(matchJsonString);
+
+        var services = new Mock<IServiceProvider>();
+        var matchApi = new Mock<IMatchApi>();
+        matchApi.Setup(api => api.GetMatch("2025iscmp_qm41", null)).Returns(match);
+        services.Setup(s => s.GetService(typeof(IMatchApi))).Returns(matchApi.Object);
+
+        // Act & Assert
+        AssertDebugException(() => webhookMessage.GetThreadDetails(services.Object), "Bad data!");
+    }
+
+    private static void AssertDebugException(Action action, string? messageContents = null)
+    {
+        try
+        {
+            action();
+        }
+        catch (Exception ex)
+        {
+            Assert.Contains("DebugAssertException", ex.GetType().ToString());
+            if (messageContents is not null)
+            {
+                Assert.Contains(messageContents, ex.Message);
+            }
+        }
     }
 }
